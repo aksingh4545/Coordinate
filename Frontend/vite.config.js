9da85a2498ae664b3c2ab34eeed639ba3d233a49
Ankit Kumar singh <aksingh4539047@gmail.com>
2025-08-22 20:32:20 +0530

    Update frontend and backend changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    // specify the output folder for your build
+    outDir: '../dist', // example: puts build files outside Frontend folder
+  },
 })
